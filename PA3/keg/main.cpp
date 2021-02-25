779d274031331a47b9d2db497824a7d6a619bebb
kristineva <>
2021-02-25 09:40:38 +0000

    keg

--- PA3/keg/main.cpp
+++ PA3/keg/main.cpp
@@ -28,15 +28,21 @@ int main(){
     
     char nodeExists[26];
     int ne_idx = 0;
-    
+
+    char letterArr[26];
+    int freqOfLetter[26];
+
     for(int i = 0; i < fileString.length(); i++){
 
         char *letterCheck = find(begin(nodeExists), end(nodeExists), fileString[i]);
 
         if (letterCheck == end(nodeExists)) {
-            nodeExists[ne_idx] = fileString[i];
-            ne_idx += 1;
             size_t counter = count(fileString.begin(), fileString.end(), fileString[i]);
+            nodeExists[ne_idx] = fileString[i];
+            letterArr[ne_idx] = fileString[i];
+            freqOfLetter[ne_idx] = counter;
+            ne_idx += 1;
+            
             node = new Node(new DataClass(fileString[i], counter));
             node_queue.push_back(node);
         }
@@ -53,6 +59,41 @@ int main(){
         }
         
     }
+
+    while(!fin.eof()){
+        fin >> lineString;
+        fileString.append(lineString);
+    }
+    
+
+    for(int i = 0; i < fileString.length(); i++){
+        countChar = count(fileString.begin(), fileString.end(), fileString[i]);
+        numOfOcc = count(uniqueString.begin(), uniqueString.end(), fileString[i]);
+
+        if (numOfOcc == 0){
+            uniqueString.append(string(1,fileString[i]));
+            node = new Node(new DataClass(fileString[i], countChar));
+
+            node_queue.push_back(node);
+        }
+    }
+
+    while(!node_queue.empty()){
+        Node *left = node_queue.front();
+        node_queue.pop_front();
+        Node *right = node_queue.front();
+        node_queue.pop_front();
+        node = new Node(NULL, left, right);
+        if(!node_queue.empty()){
+            node_queue.push_back(node);
+        }
+    }
+
+    cout << "letters";
+    copy(begin(letterArr), end(letterArr), ostream_iterator<char>(cout,"\n"));
+    cout << "freq";
+    copy(begin(freqOfLetter), end(freqOfLetter), ostream_iterator<int>(cout,"\n"));
+
 
     root = node;
 
